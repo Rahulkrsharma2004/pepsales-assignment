f7b8248190f90469e57066eca6409f45239a1f99
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2024-08-16 18:29:36 +0530

    link url

--- pepsales-app/src/features/todoSlice.jsx
+++ pepsales-app/src/features/todoSlice.jsx
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'https://pepsales-backend.onrender.com/Todos';
+const API_URL = 'https://pepsales-backend.vercel.app/todos';
 
 export const fetchTodos = createAsyncThunk('todos/fetchTodos', async () => {
   const response = await axios.get(API_URL);
